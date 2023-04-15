0978af0cf48ccbfb52aacac7d2baa0a0f35f04a9
Nipun Hedaoo <nipunhedaoo@gmail.com>
2023-04-15 18:37:49 -0400

    final

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ export default function App() {
     // <WelcomeScreen />
     // <ViewImageScreen />
     // <ListingDetailsScreens />
-    // <MessageScreen />
-    <AccountScreen />
+    <MessageScreen />
+    // <AccountScreen />
   );
 }
 

88686a3661b1c9b170c9ca2fb23a606fcc2f6c49
nipunh <nipunhedaoo@gmail.com>
2024-12-21 14:45:36 -0500

    new

--- App.js
+++ App.js
@@ -3,8 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import * as Sentry from "@sentry/react-native";
 
-export default function App() {
+Sentry.init({
+  dsn: "https://07add04aae7844ad26d275d8976a68f5@o4508507428749312.ingest.us.sentry.io/4508507835203584",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  // profilesSampleRate is relative to tracesSampleRate.
+  // Here, we'll capture profiles for 100% of transactions.
+  profilesSampleRate: 1.0,
+});
+
+
+
+const app = function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <AuthNavigator /> 
@@ -12,4 +25,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default Sentry.wrap(app);
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -14,14 +14,11 @@ export default function WelcomeScreen({ navigation }) {
                 <AppText style={{color : "white"}}>Exhange books with nearby readers</AppText>
             </View> 
             <View style={styles.buttonContainer}>
-<<<<<<< HEAD
                 <AppButton title="Login" onPress={()=> navigation.navigate(routes.LOGIN)} />
                 <AppButton title="Register" onPress={()=> navigation.navigate(routes.REGISTER)} />
-=======
                 <AppButton title="Login" onPress={()=> navigation.navigate("Login")} />
                 <AppButton title="Register" onPress={()=> navigation.navigate("Register")} />
 
->>>>>>> 64049861ed8eaf723007605eb8f152a59a0f58cd
             </View>
         </ImageBackground>
     )
